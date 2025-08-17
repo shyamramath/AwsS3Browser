2f711579eda76db31dbf4ff95b6f703aefcbf610
shyamramath <shyam.ramath@gmail.com>
2025-08-17 13:29:42 -0500

    testing the auto code review

--- my-s3-browser/src/App.jsx
+++ my-s3-browser/src/App.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { StorageBrowser } from './StorageBrowserConfig';
 
 export default function App() {
+  const setup = "setup";
+  const handleClick = () => {
+    console.log("Button clicked", setpu);
+  };
   return (
     <div style={{ margin: '10vh 15vw', textAlign: 'center' }}>
       <h1>S3 Storage Browser</h1>
+      <button onClick={handleClick}>Click Me</button>
       <StorageBrowser />
     </div>
   );
